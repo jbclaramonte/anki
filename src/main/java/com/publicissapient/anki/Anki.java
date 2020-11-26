f5b91757c2028ad9b1f6ba3ac66cda730beb18b3
Alexandre Dureau <alexandre-externe.dureau@enedis.fr>
2020-11-26 18:00:53 +0100

    évolution : test u anki.pullCard())

--- src/main/java/com/publicissapient/anki/Anki.java
+++ src/main/java/com/publicissapient/anki/Anki.java
@@ -3,16 +3,18 @@ package com.publicissapient.anki;
 import com.publicissapient.anki.domain.BoxesManager;
 import com.publicissapient.anki.domain.Deck;
 
-public class Anki {
-  private BoxesManager boxesManager = new BoxesManager();
+public class Anki
+{
+	private BoxesManager boxesManager = new BoxesManager();
 
-  public BoxesManager getBoxesManager() {
-    return boxesManager;
-  }
-  
-  
-  public void load(Deck deck)
-  {
-	  boxesManager.getRedBox().addCards(deck.getCardList());
-  }
+	public BoxesManager getBoxesManager()
+	{
+		return boxesManager;
+	}
+
+	public void load(Deck deck)
+	{
+		boxesManager.getRedBox().addCards(deck.getCardList());
+	}
+
 }
--- src/test/java/com/publicissapient/anki/AnkiTest.java
+++ src/test/java/com/publicissapient/anki/AnkiTest.java
@@ -2,30 +2,31 @@ package com.publicissapient.anki;
 
 import static org.junit.Assert.assertNotNull;
 
-import com.publicissapient.anki.domain.Card;
-import com.publicissapient.anki.domain.Deck;
-
 import org.junit.Assert;
 import org.junit.Test;
 
-public class AnkiTest 
+import com.publicissapient.anki.domain.Card;
+import com.publicissapient.anki.domain.Deck;
+
+public class AnkiTest
 {
 	@Test
 	public void test_WeHave3Boxes()
 	{
-		// Given : 
+		// Given :
 		Anki myAnki = new Anki();
-		
+
 		// When :
 		// Then :
-		
+
 		assertNotNull(myAnki.getBoxesManager().getGreenBox());
 		assertNotNull(myAnki.getBoxesManager().getOrangeBox());
 		assertNotNull(myAnki.getBoxesManager().getRedBox());
 	}
 
 	@Test
-	public void test_anki_loads_deck_in_redbox() {
+	public void test_anki_loads_deck_in_redbox()
+	{
 		// Given
 		Anki anki = new Anki();
 		Deck deck = new Deck();
@@ -38,8 +39,25 @@ public class AnkiTest
 		// Then
 		Assert.assertEquals(1, anki.getBoxesManager().getRedBox().size());
 		Assert.assertTrue(anki.getBoxesManager().getRedBox().getCards().contains(card));
-
 	}
 
+	@Test
+	public void test_anki_loads_deck_in_redbox()
+	{
+		// Given
+		Anki anki = new Anki();
+		Deck deck = new Deck();
+		Card card1 = new Card("Q1", "R1");
+		Card card2 = new Card("Q2", "R2");
+		deck.addCard(card1);
+		deck.addCard(card2);
+		anki.load(deck);
+
+		// When
+		Card myCard1 = anki.pullCard();
+
+		// Then
+		Assert.assertTrue(anki.getBoxesManager().getRedBox().getCards().contains(myCard1));
+	}
 
 }
