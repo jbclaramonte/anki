72caa539b0913beb42c8e189d05596d99956d2fc
JBC <jbclaramonte@xebia.fr>
2020-11-26 18:08:18 +0100

    WIP

--- src/test/java/com/publicissapient/anki/AnkiTest.java
+++ src/test/java/com/publicissapient/anki/AnkiTest.java
@@ -42,7 +42,7 @@ public class AnkiTest
 	}
 
 	@Test
-	public void test_anki_loads_deck_in_redbox()
+	public void test_anki_pull_card_from_redbox()
 	{
 		// Given
 		Anki anki = new Anki();
@@ -59,5 +59,27 @@ public class AnkiTest
 		// Then
 		Assert.assertTrue(anki.getBoxesManager().getRedBox().getCards().contains(myCard1));
 	}
+
+	@Test
+	public void test_reponse_for_card_is_correct()
+	{
+		// Given
+		Anki anki = new Anki();
+		Deck deck = new Deck();
+		Card card1 = new Card("Q1", "R1");
+		Card card2 = new Card("Q2", "R2");
+		deck.addCard(card1);
+		deck.addCard(card2);
+		anki.load(deck);
+		Card pullCard = anki.pullCard();
+
+		// When
+		anki.correctAnswer(pullCard);
+
+		// Then
+		Assert.assertTrue(anki.getBoxesManager().getGreenBox().getCards().contains(pullCard));
+		Assert.assertFalse(anki.getBoxesManager().getRedBox().getCards().contains(pullCard));
+	}
+
 
 }
